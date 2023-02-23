ca802cdc63ed1184000adb632c316d4843f23a8b
Egor Zublyuk <egor.zubliuk@ya.ru>
2023-02-23 16:38:10 +0600

    add example summ

--- 1_lecture/Program.cs
+++ 1_lecture/Program.cs
@@ -4,3 +4,10 @@ Console.Write("Введите имя:");
 string username = Console.ReadLine();
 Console.Write("Привет, ");
 Console.WriteLine(username);
+
+// создаём программу сложения 2-ух чисел
+int numberA = (new Random().Next(1, 99));
+Console.WriteLine(numberA);
+int numberB = (new Random().Next(1, 99));
+Console.WriteLine(numberB);
+Console.WriteLine(numberA + numberB);
